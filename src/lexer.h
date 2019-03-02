268a139fe221656d880fd74d6b3d5ff4ca560222
Vikram Waradpande <wvikram11@gmail.com>
2019-03-02 19:28:40 +0530

    Minor bugfixes and definitions in lexer

--- src/lexer.h
+++ src/lexer.h
@@ -1,38 +1,6 @@
 #include<stdio.h>
 #include "linkedlist.h"
-
-#define numStates 60
-#define numInputs 40
-#define bufferLength 500
-#define keywordHashModulo 111
-#define keywordHashMul 31
-#define maxTokenLength 15
-#define numKeywords 24
-
-
-typedef struct tInfo{
-	char* lexeme;
-	char* token;
-	int flag;
-	int lineNumber;
-}tokenInfo;
-
-typedef struct tokenInfo* TokenInfo;
-
-//typedef struct transNode* TransNode;
-
-
-
-int begin,end,state;
-char *buffer1, *buffer2,*inputBuffer;
-char** tokenList;
-int transitionTable[numStates][numInputs];
-//flag = 0 => ERROR | flag = 1+x => ACCEPT + x RETRACT
-int *stateFlags;
-
-
-//Hash table for keywords
-LinkedList* KeywordTable;
+#include "lexerDef.h"
 
 
 
@@ -44,4 +12,3 @@ char* getTokenString();
 int getTransitionIndex(char c);
 void populateTransitionTable();
 void populateTokenList();
-int currentLineNumber = 1;
